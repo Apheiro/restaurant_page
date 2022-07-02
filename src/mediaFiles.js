df31b49005f05a54ffefb12dcbb3778c52763de2
Eyen <eyentuyon@gmail.com>
2022-07-02 06:17:04 -0300

    Improve images.

--- src/mediaFiles.js
+++ src/mediaFiles.js
@@ -1,13 +1,13 @@
 import Emblem from './svg/Emblem.svg'
-import lineScroll from './svg/lineScroll.svg'
 import wave1 from './svg/waves1.svg'
 import wave2 from './svg/waves2.svg'
 import wave3 from './svg/waves3.svg'
 import wave4 from './svg/waves4.svg'
+import nextPrevBtn from './svg/nextPrevButton.svg'
 
 import friesPotatoes from './img/friesPotatoes.png'
 import hamburguers from './img/hamburguers.png'
 import hotDog from './img/hotDog.png'
 import pizza from './img/pizza.png'
 
-export { Emblem, lineScroll, wave1, wave2, wave3, wave4, friesPotatoes, hamburguers, hotDog, pizza }
+export {nextPrevBtn, Emblem, wave1, wave2, wave3, wave4, friesPotatoes, hamburguers, hotDog, pizza }
